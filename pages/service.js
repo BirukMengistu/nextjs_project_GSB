7411143d52cdaf9240fce9ec1aff64873dc4df30
Biruk  Mengistu <biruk.mengistu@learnet.se>
2021-03-23 07:29:31 +0100

    add serives pages

---
+++ pages/service.js
@@ -0,0 +1,12 @@
+import Meta from '../components/Meta'
+
+const service = () => {
+  return (
+    <div>
+      <Meta title='services' />
+      <h1>Services</h1>
+    </div>
+  )
+}
+
+export default service
